4ce8d5d53ff6c71e6ce38e4b0d04d10acca93cfe
Andre Zhang <andrezhang4246@gmail.com>
2024-11-25 20:40:16 -0600

    added debug functions print_vm_info, print_task_info, print_machine_info to help give info without using debugger

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -7,9 +7,23 @@
 
 #include "Scheduler.hpp"
 #include <assert.h>
+#include <stdio.h>
+#include <string>
 
 static bool migrating = false;
 static unsigned active_machines;
+
+static void print_vm_info(VMId_t vm);
+static void print_machine_info(MachineId_t machine);
+static void print_task_info(TaskId_t task);
+static string cpu_tostring(CPUType_t cpu);
+static string vm_type_tostring(VMType_t vm);
+static string pstate_tostring(CPUPerformance_t state);
+static string sstate_tostring(MachineState_t state);
+static string priority_tostring(Priority_t priority);
+static string sla_tostring(SLAType_t sla);
+
+
 
 void Scheduler::Init() {
     // Find the parameters of the clusters
@@ -43,6 +57,13 @@ void Scheduler::Init() {
     // SimOutput("Scheduler::Init(): VM ids are " + to_string(vms[0]) + " ahd " + to_string(vms[1]), 3);
 }
 
+/**
+ * Called by simulator when VM is done migrating due to previous call to 
+ * VM_Migrate(). When this function is called, the VM is established & can
+ * take new tasks
+ * @param time the time when the migration has been completed
+ * @param vm_id the identifier of the VM that was migrated
+ */
 void Scheduler::MigrationComplete(Time_t time, VMId_t vm_id) {
     // Update your data structure. The VM now can receive new tasks
 }
@@ -217,3 +238,286 @@ void StateChangeComplete(Time_t time, MachineId_t machine_id) {
 }
 
 
+
+/**
+ * Helper function to print all information about a given task
+ * @param task the ID of the task we want info about
+ * pre: none
+ * post: print relevant info about the task
+ */
+static void print_task_info(TaskId_t task)
+{
+    TaskInfo_t inf = GetTaskInfo(task);
+    printf("------------TASK INFO FOR ID [%u]-------------\n", task);
+    //completion status
+    printf("TASK %s\n", inf.completed ? "COMPLETED" : "UNFINISHED");
+    printf ("total instructions: %lu\n remaining: %lu\n", 
+                inf.total_instructions, inf.remaining_instructions);
+    //arrival/completion times
+    printf("time arrived: %lu\n", inf.arrival);
+    if(inf.completed){
+        printf("time completed: %lu\n", inf.completion);
+    }
+    printf("expected completion time: %lu\n", inf.target_completion);
+    //resources (gpu, memory, cpu, VM)
+    printf(inf.gpu_capable ? "GPU BOOST\n" : "NO GPU BOOST\n");
+    printf("priority: %s\n", priority_tostring(inf.priority).c_str());
+    printf("CPU type required: %s\n", cpu_tostring(inf.required_cpu).c_str());
+    printf("Memory required: %u\n", inf.required_memory);
+    printf("VM required: %s\n", vm_type_tostring(inf.required_vm).c_str());
+    printf("SLA required: %s\n", sla_tostring(inf.required_sla).c_str());
+    printf("-----------------------------------------------------\n");
+}
+
+
+
+/**
+ * Helper function to print all information about a physical machine.
+ * @param machine the id of the machine we want to print info about
+ * pre: none
+ * post: print out all auxiliary info about the machine
+ */
+static void print_machine_info(MachineId_t machine)
+{
+    MachineInfo_t inf = Machine_GetInfo(machine);
+    printf("------------MACHINE INFO FOR ID [%u]------------\n", machine);
+    //general machine info
+    printf("S-State: %s\n", sstate_tostring(inf.s_state).c_str());
+    printf("Total energy consumed: %lu\n", inf.energy_consumed);
+    //cpu info
+    printf("# of CPUs: %u\n", inf.num_cpus);
+    printf("CPU type: %s\n", cpu_tostring(inf.cpu).c_str());
+    printf("CPU P State: %s\n", pstate_tostring(inf.p_state).c_str());
+    //memory info
+    printf("Amt of memory: %u\n", inf.memory_size);
+    printf("Memory in use: %u\n", inf.memory_used);
+    //GPU 
+    printf("GPU %s\n", inf.gpus ? "ENABLED" : "DISABLED");
+    //tasks/VMs
+    printf("# active tasks: %u\n # active VMs: %u\n",
+                 inf.active_tasks, inf.active_vms);
+    printf("-----------------------------------------------\n");
+}
+
+
+
+/**
+ * Helper function to translate SLA enum to string
+ * @param sla the SLA requirement we want to translate
+ * @return the string equivalent of sla
+ */
+static string sla_tostring(SLAType_t sla)
+{
+    string ret = "UNDEFINED SLA REQUIREMENT";
+    switch(sla)
+    {
+        case SLA0:
+            ret = "SLA0";
+            break;
+        case SLA1:
+            ret = "SLA1";
+            break;
+        case SLA2:
+            ret = "SLA2";
+            break;
+        case SLA3:
+            ret = "SLA3";
+            break;
+        default:
+            break;
+    }
+    return ret;
+}
+
+
+
+/**
+ * Helper function to translate priority enum to string
+ * @param priority the enum that we want to translate to a string
+ * @return the string representation of the enum
+ */
+static string priority_tostring(Priority_t priority)
+{
+    string ret = "UNDEFINED TASK PRIORITY";
+    switch(priority)
+    {
+        case HIGH_PRIORITY:
+            ret = "HIGH PRIORITY";
+            break;
+        case MID_PRIORITY:
+            ret = "MEDIUM PRIORITY";
+            break;
+        case LOW_PRIORITY:
+            ret = "LOW PRIORITY";
+            break;
+        default:
+            break;
+    }
+    return ret;
+}
+
+
+
+/**
+ * Helper function to translate machine S-state enum into a string
+ * @param state the machine s-state
+ * @return the string equivalent of state
+ */
+static string sstate_tostring(MachineState_t state)
+{
+    string ret = "UNDEFINED MACHINE S-STATE";
+    switch(state)
+    {
+        case S0:
+            ret = "S0";
+            break;
+        case S0i1:
+            ret = "S0i1";
+            break;
+        case S1:
+            ret = "S1";
+            break;
+        case S2:
+            ret = "S2";
+            break;
+        case S3:
+            ret = "S3";
+            break;
+        case S4:
+            ret = "S4";
+            break;
+        case S5:
+            ret = "S5";
+            break;
+        default:
+            break;
+    }
+    return ret;
+}
+
+
+
+/**
+ * Helper function to translate CPU p-state enum to a string
+ * @param state the p-state we want to translate into a string
+ * @return  C++ STL string containing the p-state
+ */
+static string pstate_tostring(CPUPerformance_t state)
+{
+    string ret = "UNDEFINED CPU P-STATE";
+    switch(state)
+    {
+        case P0:
+            ret = "P0";
+            break;
+        case P1:
+            ret = "P1";
+            break;
+        case P2:
+            ret = "P2";
+            break;
+        case P3:
+            ret = "P3";
+            break;
+        default:
+            break;
+    }
+    return ret;
+}
+
+
+
+/**
+ * Helper function to print all information about a virtual machine.
+ * @param vm the id of the VM we want to print info about
+ * pre: none
+ * post: print out all auxiliary info about the VM
+ */
+static void print_vm_info (VMId_t vm)
+{
+    VMInfo_t inf = VM_GetInfo(vm);
+    printf ("------------VM Info for ID [%u]------------\n", vm);
+    //print all active tasks
+    const int N = inf.active_tasks.size();
+    printf("# active tasks: %d\n", N);
+    printf("tasks: [");
+    if(N > 0){
+        printf("%u", inf.active_tasks[0]);
+    }
+    for(int i = 1; i < N; i++){
+        printf(", %u", inf.active_tasks[i]);
+    }
+    printf("]\n");
+
+    //print cpu type
+    printf("CPU type: %s\n", cpu_tostring(inf.cpu).c_str());
+
+    //machine ID
+    printf("Machine ID: %u\n", inf.machine_id);
+
+    //vm type
+    printf("VM Type: %s\n", vm_type_tostring(inf.vm_type).c_str());
+    printf ("------------------------------------\n");
+}
+
+
+
+/**
+ * Helper function that returns the string form of the VM type enum
+ * @param vm the enum we want to convert to a string
+ * @return a C++ STL string representing the VM type
+ */
+static string vm_type_tostring(VMType_t vm)
+{
+    string ret = "UNDEFINED VM TYPE";
+    switch(vm){
+        case LINUX:
+            ret = "LINUX";
+            break;
+        case LINUX_RT:
+            ret = "LINUX_RT";
+            break;
+        case WIN:
+            ret = "WINDOWS";
+            break;
+        case AIX:
+            ret = "AIX";
+            break;
+        default:
+            break;
+    }
+    return ret;
+}
+
+
+
+/**
+ * Helper function that returns the string literal form of the cpu enum
+ * @param cpu the type of CPU we want to return for
+ * @return a C++ STL string representing the CPU type
+ */
+static string cpu_tostring (CPUType_t cpu)
+{
+    string ret = "UNDEFINED CPU TYPE";
+    switch (cpu){
+        case ARM:
+            ret  = "ARM";
+            break;
+        
+        case POWER:
+            ret = "POWER";
+            break;
+        
+        case RISCV:
+            ret = "RISCV";
+            break;
+        
+        case X86:
+            ret = "X86";
+            break;
+        
+        default:
+            break;
+    }
+    return ret;
+}
