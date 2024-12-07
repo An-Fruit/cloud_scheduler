4e7c0ac0e9ae75f5cc90d9331571a5193bb170a0
Andre Zhang <andrezhang4246@gmail.com>
2024-12-06 23:02:14 -0600

    sleep

--- SchedulerEEco.cpp
+++ SchedulerEEco.cpp
@@ -20,11 +20,12 @@
 static Scheduler Scheduler;
 static vector<MachineId_t> fully_on;
 static vector<MachineId_t> idle;
-static vector<MachineId_t> sleep;
+static vector<TaskId_t> task_queue;
 
 static unordered_map<MachineId_t, bool> changing_state;
 
 void lower_level();
+void increase_level(TaskId_t task_id);
 
 void Scheduler::Init() {
     cout << "E-Eco Scheduler!" << endl;
@@ -50,9 +51,6 @@ void Scheduler::NewTask(Time_t now, TaskId_t task_id) {
     // Find the machine with the smallest ultization (least amout of active tasks)
     for (MachineId_t id : fully_on) {
         MachineInfo_t curr_machine = Machine_GetInfo(id);
-        bool value = curr_machine.cpu == task_info.required_cpu &&
-                curr_machine.memory_size - curr_machine.memory_used >= task_info.required_memory + 8;
-        SimOutput("Machine ID: " + to_string(id), 1);
         if (curr_machine.cpu == task_info.required_cpu &&
                 curr_machine.memory_size - curr_machine.memory_used >= task_info.required_memory + 8 &&
                     !changing_state[id]) {
@@ -75,37 +73,24 @@ void Scheduler::NewTask(Time_t now, TaskId_t task_id) {
         }
     }
     if (!found_first) {
-        for (MachineId_t id : fully_on) {
-            MachineInfo_t curr_machine = Machine_GetInfo(id);
-            if (curr_machine.cpu == task_info.required_cpu) {
-                best_option = id;
-                break;
-            }
-        }
+        increase_level(task_id);
+        task_queue.push_back(task_id);
+    } else {
+        VM_Attach(new_vm, best_option);
+        VM_AddTask(new_vm, task_id, task_info.priority);
     }
-    VM_Attach(new_vm, best_option);
-    VM_AddTask(new_vm, task_id, task_info.priority);
     
 }
 
 void lower_level() {
-    for (int i = 0; i < idle.size(); i++) {
-        MachineId_t m_id = idle[i];
-        MachineInfo_t m_info = Machine_GetInfo(m_id);
-        if (idle.size() > Machine_GetTotal() * .3) {
-            Machine_SetState(m_id, S5);
-            changing_state[m_id] = true;
-            idle.erase(idle.begin() + i);
-            sleep.push_back(m_id);
-            i--;
-        }
-    }
     for (int i = 0; i < fully_on.size(); i++) {
         if (fully_on.size() == 1)
             break;
+        if (idle.size() == Machine_GetTotal() * .5)
+            break;
         MachineId_t m_id = fully_on[i];
         MachineInfo_t m_info = Machine_GetInfo(m_id);
-        if (m_info.active_tasks == 0) {
+        if (!changing_state[m_id] && m_info.active_tasks == 0) {
             Machine_SetState(m_id, S3);
             changing_state[m_id] = true;
             fully_on.erase(fully_on.begin() + i);
@@ -113,36 +98,18 @@ void lower_level() {
             i--;
         }
     }
-
 }
 
 void increase_level(TaskId_t task_id) {
     TaskInfo_t task_info = GetTaskInfo(task_id);
     for (int i = 0; i < idle.size(); i++) {
         MachineInfo_t idle_machine = Machine_GetInfo(idle[i]);
-        if (idle_machine.cpu == task_info.required_cpu && idle_machine.memory_size >= task_info.required_memory + 8) {
+        if (!changing_state[idle_machine.machine_id] && idle_machine.cpu == task_info.required_cpu 
+                && idle_machine.memory_size >= task_info.required_memory + 8) {
             Machine_SetState(idle_machine.machine_id, S0);
             changing_state[idle_machine.machine_id] = true;
             idle.erase(idle.begin() + i);
             fully_on.push_back(idle_machine.machine_id);
-            break;
-        }
-    }
-
-    for (int i = 0; i < sleep.size(); i++) {
-        MachineInfo_t sleep_machine = Machine_GetInfo(sleep[i]);
-        if (sleep_machine.cpu == task_info.required_cpu) {
-            Machine_SetState(sleep_machine.machine_id, S3);
-            changing_state[sleep_machine.machine_id] = true;
-            sleep.erase(sleep.begin() + i);
-            idle.push_back(sleep_machine.machine_id);
-            break;
-        }
-        if (i == sleep.size() - 1) {
-            Machine_SetState(sleep_machine.machine_id, S3);
-            changing_state[sleep_machine.machine_id] = true;
-            sleep.erase(sleep.begin() + i);
-            idle.push_back(sleep_machine.machine_id);
         }
     }
 }
@@ -153,8 +120,6 @@ void Scheduler::PeriodicCheck(Time_t now) {
     // SchedulerCheck is called periodically by the simulator to allow you to monitor, make decisions, adjustments, etc.
     // Unlike the other invocations of the scheduler, this one doesn't report any specific event
     // Recommendation: Take advantage of this function to do some monitoring and adjustments as necessary
-    if (now % TIMER_DECREMENT == 0)
-        lower_level();
 }
 
 void Scheduler::Shutdown(Time_t time) {
@@ -173,7 +138,8 @@ void Scheduler::TaskComplete(Time_t now, TaskId_t task_id) {
     // Do any bookkeeping necessary for the data structures
     // Decide if a machine is to be turned off, slowed down, or VMs to be migrated according to your policy
     // This is an opportunity to make any adjustments to optimize performance/energy
-    SimOutput("Scheduler::TaskComplete(): Task " + to_string(task_id) + " is complete at " + to_string(now), 4);
+    SimOutput("Scheduler::TaskComplete(): Task " + to_string(task_id) + " is complete at " + to_string(now), 1);
+    lower_level();
 }
 
 // Public interface below
@@ -232,4 +198,20 @@ void SLAWarning(Time_t time, TaskId_t task_id) {
 void StateChangeComplete(Time_t time, MachineId_t machine_id) {
     // Called in response to an earlier request to change the state of a machine
     changing_state[machine_id] = false;
+    MachineInfo_t m_info = Machine_GetInfo(machine_id);
+    if (m_info.s_state == S0) {
+        for (int i = 0; i < task_queue.size(); i++) {
+            TaskId_t t_id = task_queue[i];
+            TaskInfo_t t_info = GetTaskInfo(t_id);
+            if (t_info.required_cpu == m_info.cpu &&
+                m_info.memory_size - m_info.memory_used >= t_info.required_memory + 8) {
+                
+                VMId_t new_vm = VM_Create(t_info.required_vm, t_info.required_cpu);
+                VM_Attach(new_vm, machine_id);
+                VM_AddTask(new_vm, t_id, t_info.priority);
+                task_queue.erase(task_queue.begin() + i);
+                i--;
+            }
+        }
+    }
 }
